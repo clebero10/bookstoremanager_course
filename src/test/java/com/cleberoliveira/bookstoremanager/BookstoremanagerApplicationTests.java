0fff4a2058b545018de7f6e6dbc2326896b0d767
cleberintelicorp <145134450+cleberintelicorp@users.noreply.github.com>
2024-12-08 14:33:15 -0300

    First commit

---
+++ src/test/java/com/cleberoliveira/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleberoliveira.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
